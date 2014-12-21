5fce2311a586e5863f08ad8d3b36e3eb7d9f42d6
Patrick Tsai <patosai@outlook.com>
2014-12-21 09:55:04 -0500

    ROT decryption shows top 10 most likely rotations

--- modules/algorithms/rot.py
+++ modules/algorithms/rot.py
@@ -20,14 +20,35 @@ class ROT:
 
 	# decrypt rotates from 1-25, prints out everything
 	def decrypt(self, string):
+		# do frequency analysis
+		freqList = [0.] * 25
 		for i in range(1,26):
-			newString = self.encrypt(string, i)
-			output = "ROT {:>2}: ".format(i), newString
-			output = "".join(output)
-			if self.freqAnalysis(newString) < 0.00075:
-				output += " <---- Likely"
-			print output
-			print ""
+			freqList[i-1] = self.freqAnalysis(self.encrypt(string, i))
+		# choose the lowest 10 Euc. distances
+		for i in range(0,16):
+			freqList[freqList.index(max(freqList))] = -1
+		# display these rotations
+		print "Top Ten Most Likely Rotations"
+		print "------------------------------"
+		for i in range(0,25):
+			if freqList[i] != -1:
+				newString = self.encrypt(string, i+1)
+				output = "> {:>2}: ".format(i), newString
+				output = "".join(output)
+				print output
+				print ""
+
+		input = raw_input("(1) See all rotations?   (Anything else) Exit\t")
+		if input == "1":
+			for i in range(1,26):
+				newString = self.encrypt(string, i)
+				output = "> {:>2}: ".format(i), newString
+				output = "".join(output)
+				if self.freqAnalysis(newString) < 0.00075:
+					output += " <---- Likely"
+				print output
+				print ""
+
 
 	# encrypt rotates every character
 	def encrypt(self, string, rotConst):
@@ -55,11 +76,11 @@ class ROT:
 
 	# Performs frequency analysis on string, returns float from 0-1
 	def freqAnalysis(self, string):
-		regRatio = [0.08167, 0.01492, 0.02782, 0.04253, 0.12702,
-		0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 0.00772,
-		0.04025, 0.02406, 0.06749, 0.07507, 0.01929, 0.00095,
-		0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.02360,
-		0.00150, 0.01974, 0.00074]
+		regRatio = [0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228,
+		            0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025,
+		            0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987,
+		            0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150,
+		            0.01974, 0.00074]
 
 		string = string.replace(" ", "").lower()
 		strRatio = [0.] * 26
