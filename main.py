aedb7d144a3bee426beff169f1cdadd27d42e9ea
Patrick Tsai <patosai@outlook.com>
2014-12-20 23:22:25 -0500

    Updated package structure

--- main.py
+++ main.py
@@ -1,27 +1,12 @@
-from crypt import *
-from binhextool import *
-
-xor = XOR()
+from modules import *
 
 print "Welcome to Crypt"
 print "================"
-print ""
-
-xor = XOR()
-print "XOR Cipher"
-print "----------------"
-input = raw_input("(1) Encrypt ASCII\t(2) Decrypt Hex\t")
+input = raw_input(
+"""(1) XOR
+(2) Another option
+""")
 
 if input == "1":
-	data = raw_input("Enter data: ")
-	key = raw_input("Enter key: ")
-
-	print "Output (raw): ", xor.crypt(data, key, "chr")
-	print "Output (bin): ", xor.crypt(data, key, "bin")
-	print "Output (hex): ", xor.crypt(data, key, "hex")
-
-if input == "2":
-	data = raw_input("Enter data: ")
-	key = raw_input("Enter key: ")
-
-	print "Output: ", xor.crypt(hex_to_ascii(data), key, "chr")
+	xor = XOR()
+	xor.act()
